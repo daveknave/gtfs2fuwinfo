bceab033494570dc6bd229d8af764849e4a2b2d5
daveknave <david.roessler@fu-berlin.de>
2019-05-30 01:27:19 +0200

    + several merges

--- gtfs2fuwinfo.py
+++ gtfs2fuwinfo.py
@@ -16,17 +16,19 @@ tr_df = input_tables['trips.txt'].merge(input_tables['routes.txt'], on='route_id
 tr_df = tr_df[(tr_df['agency_id'] == 796) & (tr_df['route_type'] == 700)]
 # %%
 ts_df = tr_df.merge(input_tables['stop_times.txt'], on='trip_id', how='left')
+ts_df['stop_id'] = '0' + ts_df['stop_id'].apply(str)
 str_df = ts_df.merge(input_tables['stops.txt'], on='stop_id')
 # %%
-
+print(str_df.columns)
+# %%
 def to_edge(x, g):
     x = x.sort_values('stop_sequence')
 
     path_dist = 0
-    for pt in range(x.shape[1] - 1):
-        path_dist += hamming(x.loc[:, ['stop_lat', 'stop_lon']].iloc[pt], x.loc[:, ['stop_lat', 'stop_lon']].iloc[pt+1])
+    for pt in range(x.shape[0] - 1):
+        path_dist += hamming(list(x.loc[:, ['stop_lat', 'stop_lon']].iloc[pt]), list(x.loc[:, ['stop_lat', 'stop_lon']].iloc[pt+1]))
 
-    return pd.Series(data={
+    out_series = {
         'trip_id'       : x['trip_id'].iloc[0],
         'route_id'      : x['route_id'].iloc[0],
         'from'          : x['stop_id'].iloc[0],
@@ -34,15 +36,15 @@ def to_edge(x, g):
         'to'            : x['stop_id'].iloc[-1],
         'arr'           : x['arrival_time'].iloc[-1],
         'min_dwell'     : 0,
-        'vehicle_type'  : x['vehicle_type'].iloc[0],
+        'vehicle_type'  : x['route_type'].iloc[0],
         'backshift'     : 0,
-        'forwardshift'  : 0
-    })
-sjdf = pargroupby(gr=str_df.head(1000).groupby('trip_id'), func=to_edge, name='2edges', ncores=3)
+        'forwardshift'  : 0,
+        'distance'      : path_dist
+    }
+    return out_series
+sjdf = pargroupby(gr=str_df.groupby('trip_id'), func=to_edge, name='2edges', ncores=3)
 # %%
-print(sjdf)
-# %%
-servicejourneys = ts_df.groupby('trip_id')
+sjdf.to_csv('servicejourney.csv')
 # %%
 ### $STOPPOINTS
 
