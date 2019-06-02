7618fae76f3c634ffadfc078ee47e94345724f1a
daveknave <david.roessler@fu-berlin.de>
2019-06-02 14:13:16 +0200

    + DEADRUNTIME + CONNECTION

--- gtfs2fuwinfo.py
+++ gtfs2fuwinfo.py
@@ -2,20 +2,23 @@
 import pandas as pd
 import os, importlib
 import pargroupby
+importlib.reload(pargroupby)
 import dask
 from scipy.spatial.distance import hamming, euclidean
 import haversine
 
+# %%
+### Read GTFS data files
 data_dir = './data/GTFS'
 
 input_tables = {}
-
 for f in os.listdir(data_dir):
     tmp_df = pd.read_csv(data_dir + '/' + f, delimiter=',', decimal='.', quotechar='"')
     input_tables[f] = tmp_df
 
+### Prepare data
 tr_df = input_tables['trips.txt'].merge(input_tables['routes.txt'], on='route_id')
-tr_df = tr_df[(tr_df['agency_id'] == 796) & (tr_df['route_type'] == 700)]
+tr_df = tr_df[(tr_df['agency_id'] == 796) & (tr_df['route_type'] == 700)] ### Select BVG Bus-Services
 
 ts_df = tr_df.merge(input_tables['stop_times.txt'], on='trip_id', how='left')
 ts_df['stop_id'] = '0' + ts_df['stop_id'].apply(str)
@@ -23,7 +26,6 @@ str_df = ts_df.merge(input_tables['stops.txt'], on='stop_id').drop_duplicates()
 del ts_df
 del tr_df
 # %%
-importlib.reload(pargroupby)
 def to_edge(x, g):
     x = x.sort_values('stop_sequence')
 
@@ -43,9 +45,7 @@ def to_edge(x, g):
     }
 
 sjdf = pargroupby.do(gr=str_df.groupby('trip_id'), func=to_edge, name='2edges', ncores=4)
-# %%
-sjdf.to_csv('servicejourney.csv', index=False)
-# %%
+
 ### $SERVICEJOURNEY
 ### $SERVICEJOURNEY:ID;LineID;FromStopID;ToStopID;DepTime;ArrTime;MinAheadTime;MinLayoverTime;VehTypeGroupID;MaxShiftBackwardSeconds;MaxShiftForwardSeconds;Distance
 
@@ -68,8 +68,8 @@ servicejourney = sjdf.rename(columns={
     'forwardshift'  : 'MaxShiftForwardSeconds',
     'distance'      : 'Distance',
 })
-
-servicejourney.to_csv('servicejourney.txt')
+# %%
+servicejourney.to_csv('servicejourney.txt', index=False)
 # %%
 ### $STOPPOINTS
 ### $STOPPOINT:ID;Code;Name;VehCapacityForCharging
@@ -82,8 +82,65 @@ stoppoints = stoppoints.rename(columns={
     'stop_lat'      : 'Lat',
     'stop_lon'      : 'Lon',
 })
-stoppoints['VehCapacityForCharging'] = 1
-stoppoints.to_csv('stoppoints.txt')
+stoppoints['VehCapacityForCharging'] = 0
+
+### https://www.berlinstadtservice.de/xinh/Bus_Betriebshof_Berlin.html
+### Add depots
+stoppoints = stoppoints.append({
+    'ID'    : 900000000001,
+    'Code'  : 'DEPOT',
+    'Name'  : 'Betriebshof Weißensee',
+    'Lat'   : 52.545699,
+    'Lon'   : 13.468622,
+    'VehCapacityForCharging' : 120
+}, ignore_index=True)
+
+stoppoints = stoppoints.append({
+    'ID'    : 900000000002,
+    'Code'  : 'DEPOT',
+    'Name'  : 'Betriebshof Lichtenberg',
+    'Lat'   : 52.519746,
+    'Lon'   : 13.499957,
+    'VehCapacityForCharging' : 50
+}, ignore_index=True)
+
+stoppoints = stoppoints.append({
+    'ID'    : 900000000003,
+    'Code'  : 'DEPOT',
+    'Name'  : 'Betriebshof Wedding',
+    'Lat'   : 52.552370,
+    'Lon'   : 13.349366,
+    'VehCapacityForCharging' : 120
+}, ignore_index=True)
+
+stoppoints = stoppoints.append({
+    'ID'    : 900000000004,
+    'Code'  : 'DEPOT',
+    'Name'  : 'Betriebshof Spandau',
+    'Lat'   : 52.517266,
+    'Lon'   : 13.183191,
+    'VehCapacityForCharging' : 120
+}, ignore_index=True)
+
+stoppoints = stoppoints.append({
+    'ID'    : 900000000005,
+    'Code'  : 'DEPOT',
+    'Name'  : 'Betriebshof Neukölln',
+    'Lat'   : 52.453568,
+    'Lon'   : 13.422036,
+    'VehCapacityForCharging' : 120
+}, ignore_index=True)
+
+stoppoints = stoppoints.append({
+    'ID'    : 900000000006,
+    'Code'  : 'DEPOT',
+    'Name'  : 'Betriebshof Wilmersdorf',
+    'Lat'   : 52.494360,
+    'Lon'   : 13.301960,
+    'VehCapacityForCharging' : 120
+}, ignore_index=True)
+
+stoppoints.to_csv('stoppoints.txt', index=False)
 # %%
 ### $LINE
 ### $LINE:ID;Code;Name
@@ -93,17 +150,22 @@ line = line.rename(columns={
     'route_short_name'      : 'Code',
 })
 line['Name'] = line['Code']
-line.to_csv('line.txt')
-# %%
-sp_red = stoppoints[(stoppoints['ID'].isin(sjdf['from'])) | (stoppoints['ID'].isin(sjdf['to']))]
-print(sp_red.shape)
+line.to_csv('line.txt', index=False)
+
 # %%
 ### $DEADRUNTIME
 ### $DEADRUNTIME:FromStopID;ToStopID;FromTime;ToTime;Distance;RunTime
-# Create Deadheadmatrix
+
+stoppoints = pd.read_csv('stoppoints.txt')
+
+sjdf = pd.read_csv('servicejourney.txt')
+sp_red = stoppoints[(stoppoints['ID'].isin(sjdf['FromStopID'])) | (stoppoints['ID'].isin(sjdf['ToStopID'])) | (stoppoints['Code'] == 'DEPOT')]
+
+# Create Deadhead matrix
 sp_red['key'] = 1
 crossprod = sp_red.merge(sp_red, on="key")
-crossprod['distance'] = crossprod[crossprod['ID_x'] != crossprod['ID_y']].apply(lambda x: haversine.haversine([x['Lat_x'],x['Lon_x']],[x['Lat_y'],x['Lon_y']], unit=haversine.Unit.METERS), axis=1)
+crossprod['distance'] = crossprod[crossprod['ID_x'] != crossprod['ID_y']].\
+    apply(lambda x: haversine.haversine([x['Lat_x'],x['Lon_x']],[x['Lat_y'],x['Lon_y']], unit=haversine.Unit.METERS), axis=1)
 
 crossprod = crossprod.rename(columns={
     'ID_x'          : 'FromStopID',
@@ -114,5 +176,24 @@ crossprod['FromTime'] = 0
 crossprod['ToTime'] = 0
 crossprod['RunTime'] = 60 * crossprod['Distance'] / 25
 crossprod = crossprod[['FromStopID','ToStopID','FromTime','ToTime','Distance','RunTime']].drop_duplicates().dropna()[crossprod['Distance'] < 3000]
-crossprod.to_csv('deadruntime.txt')
+crossprod.to_csv('deadruntime.txt', index=False)
 # %%
+### $CONNECTIONS
+### https://developers.google.com/transit/gtfs/reference/#transferstxt
+### $CONNECTIONS
+### $CONNECTIONS:FromStopID;ToStopID;FromLineID;ToLineID;MinTransferTime
+
+connections = input_tables['transfers.txt'].copy()
+connections = connections[ (connections['from_stop_id'].isin(stoppoints['ID'])) & (connections['to_stop_id'].isin(stoppoints['ID'])) & (connections['transfer_type'] == 1)]
+
+connections = connections.rename(columns={
+    'from_stop_id'          : 'FromStopID',
+    'to_stop_id'            : 'ToStopID',
+    'from_route_id'         : 'FromLineID',
+    'to_route_id'           : 'ToLineID',
+    'min_transfer_time'     : 'MinTransferTime',
+})
+
+connections[['FromStopID','ToStopID','FromLineID','ToLineID','MinTransferTime']].to_csv('connections.txt', index=False)
+# %%
+
